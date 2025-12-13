dc776fee6ba326099a7cb829dd24610a2fa95fc6
akshaya2007-hari <angalaeswari.m2007@gmail.com>
2025-12-13 21:32:19 +0530

    Update app1.py

--- app1.py
+++ app1.py
@@ -4,7 +4,7 @@ import numpy as np
 
 st.set_page_config(page_title="Fuel Prediction App")
 
-with open("fuel22.pkl", "rb") as f:
+with open("fuel.pkl", "rb") as f:
     model = pickle.load(f)
 
 st.title("⛽ Fuel Prediction App")
@@ -15,4 +15,5 @@ if st.button("Predict"):
     input_data = np.array([[x_value]])
     prediction = model.predict(input_data)
     st.success(f"Prediction: {float(prediction[0]):.2f}")
+
 
